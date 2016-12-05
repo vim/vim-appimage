7ece37412b85ec9c917fe41c83eb439efe8338fa
Christian Brabandt <cb@256bit.org>
2016-12-05 17:03:53 +0100

    Add libncurses5 to image

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -54,6 +54,8 @@ find "${SOURCE_DIR}" -name "vim48x48.png" -exec cp {} "${LOWERAPP}.png" \;
 mkdir -p ./usr/lib/x86_64-linux-gnu
 # copy custom libruby.so 1.9
 find $HOME/.rvm/ -name "libruby.so.1.9" -exec cp {} ./usr/lib/x86_64-linux-gnu/ \;
+# add libncurses5
+find / -name "libncurses.so.5" -exec cp -v -rfL {} ./usr/lib/x86_64-linux-gnu/ \;
 
 # copy dependencies
 copy_deps
