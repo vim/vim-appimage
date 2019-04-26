5efa9573bf8b5920f95c2c1739f269daa7d5dc32
Ralf Schandl <ralf.schandl@de.ibm.com>
2019-04-27 01:02:27 +0200

    Use AppImage ARGV0 to decide between GVim or Vim

--- scripts/gvim.start.sh
+++ scripts/gvim.start.sh
@@ -2,6 +2,22 @@
 
 export VIM=$APPDIR/usr/share/vim
 export VIMRUNTIME=$APPDIR/usr/share/vim/vim81
-cd $OWD
-exec gvim -f "$@"
+
+# OWD - Original Working Directory - set by AppImage
+# see https://github.com/AppImage/AppImageKit/blob/master/src/runtime.c
+cd "$OWD"
+
+if [ -n "$ARGV0" ]; then
+    basename="$(basename "$ARGV0" | tr 'A-Z' 'a-z')"
+else
+    basename="$(basename "$APPIMAGE" | tr 'A-Z' 'a-z')"
+fi
+case $basename in
+    vim*)
+        exec vim "$@"
+        ;;
+    *)
+        exec gvim -f "$@"
+        ;;
+esac
 
