68bdb4ae208cb41734939d974fc2a5d8c2eb674a
Ralf Schandl <ralf.schandl@de.ibm.com>
2019-04-28 13:15:19 +0200

    Build AppImage that could start a Terminal Vim
    The AppImage now doesn't start GVim directly, but calls a script
    to start Vim. This script evaluates the name that was used to start
    the AppImage. If the name starts with 'vim', the terminal version is
    started, else GVim is started.
    
    Prerequisite for this was the use of AppIMageKit V11 (newest release).
    
    Also added the script 'scripts/build_vim.sh' that is used to configure
    and make Vim. This script is also used from '.travis.yml'.

---
+++ scripts/vim.start.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ -z "$VIM" ]; then
+    export VIM=$APPDIR/usr/share/vim
+fi
+if [ -z "$VIMRUNTIME" ]; then
+    export VIMRUNTIME=$APPDIR/usr/share/vim/vim81
+fi
+
+# We don't pack Python stuff, so unset the PYTHON vars set by AppRun.
+# see https://github.com/AppImage/AppImageKit/blob/master/src/AppRun.c
+unset PYTHONPATH
+unset PYTHONHOME
+unset PYTHONDONTWRITEBYTECODE
+
+# Change back to OWD - Original Working Directory
+# Set by AppImage
+# see https://github.com/AppImage/AppImageKit/blob/master/src/runtime.c
+cd "$OWD"
+
+# ARGV0 should always be set, if not fall back to AppImage name
+if [ -n "$ARGV0" ]; then
+    basename="$(basename "$ARGV0" | tr '[:upper:]' '[:lower:]')"
+else
+    basename="$(basename "$APPIMAGE" | tr '[:upper:]' '[:lower:]')"
+fi
+case $basename in
+    vim*)
+        exec vim "$@"
+        ;;
+    *)
+        exec gvim -f "$@"
+        ;;
+esac
