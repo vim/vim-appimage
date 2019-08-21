aa158ca7d053f5f1557d9a12af738984f6ff2ddb
Christian Brabandt <cb@256bit.org>
2019-08-21 08:46:58 +0200

    scripts/dump: Check existence of failed dir before trying to access it

--- scripts/dump_failed_screentests.sh
+++ scripts/dump_failed_screentests.sh
@@ -4,4 +4,6 @@
 #
 # this is in an extra shell script, because Travis fails to parse the following line :(
 
-find vim/src/testdir/failed -type f -name "*.dump" -size +0 -exec sh -c 'for i; do echo "failed: " $i && cat $i && echo ""; done'  find-sh {} +
+if [ -d vim/src/testdir/failed ]; then
+  find vim/src/testdir/failed -type f -name "*.dump" -size +0 -exec sh -c 'for i; do echo "failed: " $i && cat $i && echo ""; done'  find-sh {} +
+fi
