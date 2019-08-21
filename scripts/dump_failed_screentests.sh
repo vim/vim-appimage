3ba277758a8d3888756915da8b222a897db5eb06
Christian Brabandt <cb@256bit.org>
2019-08-21 08:33:48 +0200

    Add script to dump failed screen tests

---
+++ scripts/dump_failed_screentests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Finds and dumps failed screen tests from the vim repository
+#
+# this is in an extra shell script, because Travis fails to parse the following line :(
+
+find vim/src/testdir/failed -type f -name "*.dump" -size +0 -exec sh -c 'for i; do echo "failed: " $i && cat $i && echo ""; done'  find-sh {} +
