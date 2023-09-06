a4e5986d3ef8bf7dab14f242e24e33adffe90807
Christian Brabandt <cb@256bit.org>
2023-09-06 14:49:23 +0200

    Rework the Appimage Generation
    This uses the linuxdeploy script and simplifies the creation of Appimage
    greatly.
    
    It also hopefully fixes two issues: #51 and possibly a security relevant
    one, mentioned on huntr.
    
    Let's test

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
 
-########################################################################
-# Package the binaries built on GitHub Actions as an AppImage
-# By Simon Peter 2016
-# For more information, see http://appimage.org/
-########################################################################
-
 set -e
+
+patch_desktop_files()
+{
+	# Remove duplicate keys from desktop file. This might occure while localisation
+	# for the desktop file is progressing.
+	pushd "${SOURCE_DIR}/vim/runtime"
+	mv ${LOWERAPP}.desktop ${LOWERAPP}.desktop.orig
+	awk '{x=$0; sub(/=.*$/, "", x);if(!seen[x]++){print $0}}' ${LOWERAPP}.desktop.orig > ${LOWERAPP}.desktop
+	rm ${LOWERAPP}.desktop.orig
+
+	if [[ "$LOWERAPP" == "vim" ]]; then
+		sed -i "s/^Icon=gvim/Icon=vim/" ${LOWERAPP}.desktop
+	fi
+	find . -xdev -name "vim48x48.png" -exec cp {} "${LOWERAPP}.png" \;
+	popd
+}
+
+make_appimage()
+{
+	cd ${BUILD_BASE}
+	test -x ./linuxdeploy.appimage || ( \
+		wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage \
+			-O linuxdeploy.appimage \
+		&& chmod +x linuxdeploy.appimage )
+
+	export UPDATE_INFORMATION="gh-releases-zsync|vim|vim-appimage|latest|$APP-*x86_64.AppImage.zsync"
+	export OUTPUT="${APP}-${VERSION}.glibc${glibc}-${ARCH}.AppImage"
+
+	./linuxdeploy.appimage --appdir "$APP.AppDir" \
+		-d "${SOURCE_DIR}/vim/runtime/${LOWERAPP}.desktop" \
+		-i "${SOURCE_DIR}/vim/runtime/${LOWERAPP}.png" \
+		--output appimage
+
+	if [ -n "$GITHUB_ACTIONS" ]; then
+			TARGET_NAME=$(find "$BUILD_BASE/" -type f -name "$APP-*.AppImage" -printf '%f\n')
+			echo "Copy $BUILD_BASE/$TARGET_NAME -> $GITHUB_WORKSPACE"
+			cp "$BUILD_BASE/$TARGET_NAME" "$GITHUB_WORKSPACE"
+			cp "$BUILD_BASE/$TARGET_NAME.zsync" "$GITHUB_WORKSPACE"
+	fi
+}
 
 if [ -z "$1" ]; then
   APP=GVim
 else
   APP=$1
 fi
-LOWERAPP=${APP,,}
-
-ARCH=$(arch)
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
-
 if [ -n "$GITHUB_ACTIONS" ]; then
     echo "GitHub Actions detected"
     BUILD_BASE=$HOME
@@ -27,152 +57,26 @@ else
     mkdir -p "$BUILD_BASE"
 fi
 
-# Did never work
-#TAG=$(git describe --exact-match --tags HEAD 2> /dev/null)
-#RE="^untagged-.*"
-#if [[ $? != 0  ||  "$TAG" =~ "$RE" ]]; then
-#    echo  "non-tag commit"
-#    exit
-#fi
+GIT_REV="$(git rev-parse --short HEAD)"
+VERSION="$(git describe --tags --abbrev=0)"
+SOURCE_DIR="$(git rev-parse --show-toplevel)"
+ARCH=$(arch)
+LOWERAPP=${APP,,}
 
 # uses the shadowdir from build_vim.sh
 cd vim/src/$LOWERAPP
+GLIBC=$(find ${SOURCE_DIR} -type f -executable -exec strings {} \; | grep "^GLIBC_2" | sed s/GLIBC_//g | sort --version-sort | uniq | tail -n 1)
+# Prepare some source files
+patch_desktop_files
 
-GIT_REV="$(git rev-parse --short HEAD)"
+make install DESTDIR="${BUILD_BASE}/${APP}.AppDir" >/dev/null
 
-VERSION="$(git describe --tags --abbrev=0)"
+# Create Appimage
+make_appimage
 
-SOURCE_DIR="$(git rev-parse --show-toplevel)"
-make install DESTDIR="$BUILD_BASE/$APP/$APP.AppDir"
-
-cd "$BUILD_BASE/$APP/"
-
-wget -q https://github.com/AppImage/pkg2appimage/raw/master/functions.sh -O ./functions.sh
-. ./functions.sh
-
-cd "$APP.AppDir"
-
-# Also needs grep for gvim.wrapper
-cp /bin/grep ./usr/bin
-
-# install additional dependencies for python
-# this makes the image too big, so skip it
-# and depend on the host where the image is run to fulfill those dependencies
-
-#URL=$(apt-get install -qq --yes --no-download --reinstall --print-uris libpython2.7 libpython3.2 libperl5.14 liblua5.1-0 libruby1.9.1| cut -d' ' -f1 | tr -d "'")
-#wget -c $URL
-#for package in *.deb; do
-#    dpkg -x $package .
-#done
-#rm -f *.deb
-
-
-########################################################################
-# Copy desktop and icon file to AppDir for AppRun to pick them up
-########################################################################
-
-get_apprun
-
-get_desktop
-
-find "${SOURCE_DIR}" -xdev -name "vim48x48.png" -exec cp {} "${LOWERAPP}.png" \;
-
-mkdir -p ./usr/lib/x86_64-linux-gnu
-
-if [[ "$LOWERAPP" == "gvim" ]]; then
-  # package the gio plugins for the GUI
-  cp -a /usr/lib/x86_64-linux-gnu/gio ./usr/lib/x86_64-linux-gnu/
-fi
-
-# copy dependencies
-copy_deps
-
-# Move the libraries to usr/bin
-move_lib
-
-########################################################################
-# Delete stuff that should not go into the AppImage
-########################################################################
-
-# include libpango, to avoid undefined symbol g_memdup2 on Ubuntu 22.04
-#find . -name "libpango*" -delete
-find . -name "libfreetype*" -delete
-find . -name "libX*" -delete
-
-# Delete dangerous libraries; see
-# https://github.com/probonopd/AppImages/blob/master/excludelist
-delete_blacklisted
-
-########################################################################
-# Patch away absolute paths; it would be nice if they were relative
-########################################################################
-
-sed -i -e "s|/usr/share/|././/share/|g" usr/bin/vim
-sed -i -e "s|/usr/lib/|././/lib/|g" usr/bin/vim
-sed -i -e "s|/usr/share/doc/vim/|././/share/doc/vim/|g" usr/bin/vim
-
-# Possibly need to patch additional hardcoded paths away, replace
-# "/usr" with "././" which means "usr/ in the AppDir"
-
-# remove unneeded stuff
-rmdir ./usr/lib64 || true
-rm -rf ./usr/bin/*tutor* || true
-rm -rf ./usr/share/doc || true
-#rm -rf ./usr/bin/vim || true
-# remove unneeded links
-find ./usr/bin -type l \! -name "gvim" -delete || true
-
-########################################################################
-# Patch gvim.desktop file and copy start script
-########################################################################
-
-# Remove duplicate keys from desktop file. This might occure while localisation
-# for the desktop file is progressing.
-mv ${LOWERAPP}.desktop ${LOWERAPP}.desktop.orig
-awk '{x=$0; sub(/=.*$/, "", x);if(!seen[x]++){print $0}}' ${LOWERAPP}.desktop.orig > ${LOWERAPP}.desktop
-rm ${LOWERAPP}.desktop.orig
-
-if [[ "$LOWERAPP" == "vim" ]]; then
-  # This confuses generate_type2_appimage, so get rid of it for Vim
-  rm gvim.desktop
-  sed -i "s/^Icon=gvim/Icon=vim/" ${LOWERAPP}.desktop
-fi
-
-# change Exec line to script
-sed -i "s/^Exec=${LOWERAPP}.*$/Exec=vim.start.sh %F/" ${LOWERAPP}.desktop
-
-# copy script
-cp "$script_dir/vim.start.sh"  ./usr/bin
-chmod +x ./usr/bin/vim.start.sh
-
-########################################################################
-# AppDir complete
-# Now packaging it as an AppImage
-########################################################################
-
-cd "$BUILD_BASE/$APP" # Go out of AppImage
-
-generate_type2_appimage -u "gh-releases-zsync|vim|vim-appimage|latest|$APP-*x86_64.AppImage.zsync"
-
-if [ -n "$GITHUB_ACTIONS" ]; then
-    TARGET_NAME=$(find "$BUILD_BASE/out" -type f -name "$APP-*.AppImage" -printf '%f\n')
-    echo "Copy $BUILD_BASE/out/$TARGET_NAME -> $GITHUB_WORKSPACE"
-    cp "$BUILD_BASE/out/$TARGET_NAME" "$GITHUB_WORKSPACE"
-    cp "$BUILD_BASE/out/$TARGET_NAME.zsync" "$GITHUB_WORKSPACE"
-fi
-
-########################################################################
 # Create Github Release
-########################################################################
-
 if [ -n "$GITHUB_ACTIONS" ]; then
   pushd "$script_dir"
   . release_notes.sh > "$GITHUB_WORKSPACE/release.body"
   popd
 fi
-
-########################################################################
-# Upload the AppDir
-########################################################################
-
-#transfer ../out/*
